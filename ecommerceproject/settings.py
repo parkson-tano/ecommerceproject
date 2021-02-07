184ec6f46c60e711ee7a73449c566e7b94bca767
King Dani <tanoparksonsilencer@gmail.com>
2021-02-07 18:12:11 +0100

    Update settings.py

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v#)chd(m$8-1*dbazlgvii--__!or(#f7mrscuarg9tt0f@7@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["danitano.herokuapp.com", '127.0.0.1']
 
 
 # Application definition
