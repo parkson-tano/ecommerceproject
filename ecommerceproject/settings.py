ced3f3d8c14165368ac701f49a71b7ddabddab21
King Dani <tanoparksonsilencer@gmail.com>
2021-02-08 11:14:02 +0100

    postgre db fix

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -80,16 +80,28 @@ WSGI_APPLICATION = 'ecommerceproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'test',
+        'USER': 'postgres',
+        'PASSWORD' : 'P@$$w0rd',
+        'HOST': 'localhost',
+        'PORT' : '5432',
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
